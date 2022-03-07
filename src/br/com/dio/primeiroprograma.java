3cf1f30ba5045abe3c3a31688267e230a18b7628
laercio <lac.trigo@gmail.com>
2022-03-07 13:42:42 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		livros livros = new livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+		/*int a = 2;
+		int b = 3;		
+		System.out.println("helo world"   + (a+b));*/
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class livros {
+	private String nome;
+	private String npag;
+	
+}
